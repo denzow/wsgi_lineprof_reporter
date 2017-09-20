77bd44075b2eca8f856433f351eb32df712a4616
denzow <denzow@gmail.com>
2017-09-20 23:17:22 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
-# wsgi_lineprof_reporter
-parse and report for "wsgi_lineprof"
+wsgi_lineprof_reporter
+============================
+
+このスクリプトは[wsgi_lineprof](https://github.com/ymyzk/wsgi_lineprof)で出力した複数の結果を統合し、サマリー的なレポートを出力します。
+[wsgi_lineprof](https://github.com/ymyzk/wsgi_lineprof)では1リクエストごとにログが出力されますので、大量にアクセスしたあとでは確認量が多く時間がかかっていました。
+そのためすべての結果を集計してシンプルなレポートを出力します。
+
+
+前提
+------------
+
+本スクリプトはPython3.xの標準ライブラリのみで作成しているので依存ライブラリはありません。(おそらく)Python3が入った全ての環境で動作します。
+
+
+
+インストール
+------------
+
+まだ**PyPi**には登録できていません。リポジトリをそのままコピーされるか以下の様にスクリプトをそのまま取得してください。
+
+```
+$ curl https://github.com/denzow/wsgi_lineprof_reporter/blob/master/wlreporter.py
+```
+
+使い方
+-----------
+
+
+
+[wsgi_lineprof](https://github.com/ymyzk/wsgi_lineprof)で対象のアプリケーションのプロファイルログを取得します。。
+取得したファイルに対して以下のように実行します。
+
+```bash
+$ python wlreporter.py -f /target/file/name.log
+```
+
+実行すると以下の2つのファイルが元ファイルと同じディレクトリに生成されます。
+
+* `xxxx_summary_data.log`
+* `xxxx_line_data.log`
+
+それぞれ以下のような内容です。
+
+
+#### `xxxx_summary_data.log`
+
+関数単位での実行時間順に集計した結果です。上位の関数にチューニングポイントがあります。
+
+```
+                         file_name       func_name  total_time  avg_time  call_count
+----------------------------------  --------------  ----------  --------  ----------
+/home/XXXXXXXXXXXXXX/python/app.py  get_index          0.17849   0.17849           1
+/home/XXXXXXXXXXXXXX/python/app.py  get_initialize         0.0       0.0           1
+
+```
+
+#### `xxxx_line_data.log`
+
+スクリプト内の各行ごとにかかった時間やHit等を集計しています。
+
+
+```
+                         file_name  line  hits  total_time       graph                                                                                                         code
+----------------------------------  ----  ----  ----------  ----------  -----------------------------------------------------------------------------------------------------------
+/home/XXXXXXXXXXXXXX/python/app.py   192     0           0              @app.route('/')                                                                                            
+/home/XXXXXXXXXXXXXX/python/app.py   193     0           0              def get_index():                                                                                           
+/home/XXXXXXXXXXXXXX/python/app.py   194     1         191                  page = int(request.args.get('page', 0))                                                                
+/home/XXXXXXXXXXXXXX/python/app.py   195     1        2333                  cur = db().cursor()                                                                                    
+/home/XXXXXXXXXXXXXX/python/app.py   196     1           1                  cur.execute("""                                                                                        
+/home/XXXXXXXXXXXXXX/python/app.py   197     0           0                  SELECT                                                                                                 
+/home/XXXXXXXXXXXXXX/python/app.py   198     0           0                  id,                                                                                                    
+/home/XXXXXXXXXXXXXX/python/app.py   199     0           0                  name,                                                                                                  
+/home/XXXXXXXXXXXXXX/python/app.py   200     0           0                  image_path,                                                                                            
+/home/XXXXXXXXXXXXXX/python/app.py   201     0           0                  price,                                                                                                 
+/home/XXXXXXXXXXXXXX/python/app.py   202     0           0                  description,                                                                                           
+/home/XXXXXXXXXXXXXX/python/app.py   203     0           0                  created_at                                                                                             
+/home/XXXXXXXXXXXXXX/python/app.py   204     0           0                  FROM                                                                                                   
+/home/XXXXXXXXXXXXXX/python/app.py   205     0           0                      products p                                                                                         
+/home/XXXXXXXXXXXXXX/python/app.py   206     0           0                  ORDER BY  id DESC LIMIT 50 OFFSET {}                                                                   
+/home/XXXXXXXXXXXXXX/python/app.py   207     1        2580                  """.format(page * 50))                                                                                 
+/home/XXXXXXXXXXXXXX/python/app.py   208     1           9                  products = cur.fetchall()                                                                              
+/home/XXXXXXXXXXXXXX/python/app.py   209     1          54                  product_id_list = [str(x["id"]) for x in products]                                                     
+/home/XXXXXXXXXXXXXX/python/app.py   210     1       29017  **              comment_data = get_all_comments(product_id_list)                                                       
+/home/XXXXXXXXXXXXXX/python/app.py   212    51          27                  for product in products:                                                                               
+/home/XXXXXXXXXXXXXX/python/app.py   213    50          64                      product['description'] = product['description'][:70]                                               
+/home/XXXXXXXXXXXXXX/python/app.py   214    50         183                      product['created_at'] = to_jst(product['created_at'])                                              
+/home/XXXXXXXXXXXXXX/python/app.py   215    50          50                      product['comments'] = comment_data.get(product['id'])[:5]#get_comments(product['id'])              
+/home/XXXXXXXXXXXXXX/python/app.py   216    50          42                      product['comments_count'] = len(comment_data.get(product['id'])) #get_comments_count(product['id'])
+/home/XXXXXXXXXXXXXX/python/app.py   218     1      143940  **********      return render_template('index.html', products=products, current_user=current_user())                   
+/home/XXXXXXXXXXXXXX/python/app.py   278     0           0              @app.route('/initialize')                                                                                  
+/home/XXXXXXXXXXXXXX/python/app.py   279     0           0              def get_initialize():                                                                                      
+/home/XXXXXXXXXXXXXX/python/app.py   280     1        9837  *               cur = db().cursor()                                                                                    
+/home/XXXXXXXXXXXXXX/python/app.py   281     1         284                  cur.execute('DELETE FROM users WHERE id > 5000')                                                       
+/home/XXXXXXXXXXXXXX/python/app.py   282     1         225                  cur.execute('DELETE FROM products WHERE id > 10000')                                                   
+/home/XXXXXXXXXXXXXX/python/app.py   283     1        3487                  cur.execute('DELETE FROM comments WHERE id > 200000')                                                  
+/home/XXXXXXXXXXXXXX/python/app.py   284     1        4803                  cur.execute('DELETE FROM histories WHERE id > 500000')                                                 
+/home/XXXXXXXXXXXXXX/python/app.py   285     1           0                  return ("Finish")       
+
+```
+
+graph列はレポート内の最大時間との比率を`*`で表現しています。`*`が多い行にチューニングポイントがあります。
+
+
+#### その他オプション
+
+
+幾つかオプションがあります。
+
+```
+denzownoMacBook-Pro:wsgi_lineprof_reporter denzow$ python wlreporter.py --help
+usage: wlreporter.py [-h] -f TARGET_FILE [-d DB_NAME] [-r REPORT_NAME_PREFIX]
+
+This script is parsing wsgi_lineprof result
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f TARGET_FILE, --file TARGET_FILE
+                        target line profiler log file
+  -d DB_NAME, --db-name DB_NAME
+                        db name for persistence. if not set, use :memory:.
+                        :memory: is temporary database.
+  -r REPORT_NAME_PREFIX, --report-name-prefix REPORT_NAME_PREFIX
+                        report name prefix. if not set, use profile log name.
+```
+
+内部では一旦オンメモリのSQLiteに格納してからレポートティングしています。そのためレポート後は自動で
+DBは破棄されますが`-d`でファイル名を指定することで分析結果を格納したDBをスクリプト終了後も保持することができます。
+
+通常のレポート以上の分析を実施する場合はこちらのDBをSQLiteに対応したクライアントで分析してください。
+
+謝辞
+---------
+
+本スクリプトは[wsgi_lineprof](https://github.com/ymyzk/wsgi_lineprof)を前提としています。[wsgi_lineprof](https://github.com/ymyzk/wsgi_lineprof)の作者のymyzk様の
+お陰でWEBアプリのパフォーマンスチェックが快適になりました。
+
---
+++ wlreporter.py
@@ -0,0 +1,421 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+this script is creating more useful report for ISUCON from line profiler log bia **wsgi_lineprof**.
+
+"""
+
+
+import argparse
+import re
+import sqlite3 as sqlite
+
+
+class DbClass(object):
+    """
+    init sqlite for analyze.
+    """
+
+    def __init__(self, db_name):
+        """
+
+        :param str db_name:
+        """
+        self.db_name = db_name
+        self.conn = sqlite.connect(self.db_name)
+        self.conn.row_factory = sqlite.Row
+        self.conn.execute("""
+        create table profile_data(
+            id  integer PRIMARY KEY AUTOINCREMENT,
+            file_name text,
+            func_name text,
+            total_time float
+        )
+        """)
+        self.conn.execute("""
+        create table profile_line_data(
+            id  INTEGER PRIMARY KEY AUTOINCREMENT,
+            profile_id integer,
+            file_name text,
+            func_name text,
+            line integer,
+            hits integer,
+            time integer,
+            code text
+        )
+        """)
+        self.cursor = self.conn.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+    def create_indexes(self):
+        """
+        add indexes
+        :return:
+        """
+        self.execute("create index idx_profile_data_file_name on profile_data(file_name)")
+        self.execute("create index idx_profile_data_func_name on profile_data(func_name)")
+
+        self.execute("create index idx_profile_line_data_file_name on profile_line_data(file_name)")
+        self.execute("create index idx_profile_line_data_func_name on profile_line_data(func_name)")
+        self.execute("create index idx_profile_line_data_line on profile_line_data(line)")
+
+    def commit(self):
+        self.conn.commit()
+
+    def execute(self, sql, params=None):
+        """
+        execute sql
+
+        :param str sql:
+        :param tuple params:
+        :return cursor:
+        """
+        if params:
+            return self.cursor.execute(sql, params)
+        else:
+            return self.cursor.execute(sql)
+
+    def save_profile_data(self, file_name, func_name, total_time):
+        """
+        save function based profile data.
+
+        :param str file_name: python script name
+        :param str func_name: function name
+        :param str total_time: total time
+        :return int: sequence for profile data.
+        """
+        self.execute("""
+        insert into profile_data(file_name, func_name, total_time)
+        values(?,?,?)
+        """, (file_name, func_name, total_time))
+
+        return self.cursor.lastrowid
+
+    def save_line_data(self, profile_id, file_name, func_name, line, hits, time, code):
+        """
+        save line based profile data.
+
+        :param int profile_id: from save_profile_data()
+        :param str file_name: python script name
+        :param str func_name: function name
+        :param str line: line no
+        :param str hits: hits num
+        :param str time: time
+        :param str code: code string
+        :return:
+        """
+        self.execute("""
+            insert into profile_line_data(profile_id, file_name, func_name, line, hits, time, code)
+            values(?,?,?,?,?,?,?)
+        """, (profile_id, file_name, func_name, line, hits, time, code))
+
+    # debug
+    def _dump(self):
+        for row in self.execute("select * from profile_data"):
+            print(list(row))
+
+    # debug
+    def _dump_line(self):
+        for row in self.execute("select * from profile_line_data"):
+            print(list(row))
+
+
+def init():
+    """
+    script initialize. get arguments
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='This script is parsing wsgi_lineprof result')
+    parser.add_argument(
+        "-f",
+        "--file",
+        required=True,
+        dest="target_file",
+        help="target line profiler log file"
+    )
+    parser.add_argument(
+        "-d",
+        "--db-name",
+        dest="db_name",
+        default=":memory:",
+        help="db name for persistence. if not set, use :memory:. :memory: is temporary database."
+    )
+
+    parser.add_argument(
+        "-r",
+        "--report-name-prefix",
+        dest="report_name_prefix",
+        help="report name prefix. if not set, use profile log name."
+    )
+
+    args = parser.parse_args()
+
+    return args.target_file, args.db_name, args.report_name_prefix
+
+
+def parse(profiler_log_name):
+    """
+    parse line profile report.
+    :param str profiler_log_name:
+    :return:
+    """
+
+
+    """
+    File: /home/ishocon/webapp/python/app.py
+    Name: get_mypage
+    Total time: 0.172644 [sec]
+      Line      Hits         Time  Code
+    ===================================
+       221                         @app.route('/users/<int:user_id>')
+       222                         def get_mypage(user_id):
+       223         1        12230      cur = db().cursor()
+       224         1            1      cur.execute(""
+       225                         SELECT p.id, p.name, p.description, p.image_path, ifnull(p.price,0) as price , h.created_at
+       226                         FROM histories as h
+    :
+       249         1        23970                                            total_pay=total_pay, current_user=current_user())
+    
+    File:
+    """
+    empty_pattern = re.compile("^\s*$")
+    file_pattern = re.compile("^File: (.+)")
+    name_pattern = re.compile("^Name: (.+)")
+    time_pattern = re.compile("^Total time: (.+) \[sec\]")
+    # code have indent, so get another way.
+    line_pattern = re.compile("^\s+(\d+)\s+(\d+)?\s+(\d+)?\s+(.+)")
+
+    is_data = False
+    is_line_founded = False
+    # code column start point , because code is not start with space.
+    code_index = 0
+    buf = {
+        "file_name": None,
+        "func_name": None,
+        "total_time": None,
+        "lines": [],
+    }
+
+    profile_data_list = []
+    for line in open(profiler_log_name):
+        # not need \n
+        line = line.rstrip("\n")
+
+        # start data block
+        if file_pattern.match(line):
+            is_data = True
+
+        # end data block and clear.
+        if is_data and empty_pattern.match(line):
+            is_data = False
+            is_line_founded = False
+            profile_data_list.append(buf)
+            buf = {
+                "file_name": None,
+                "func_name": None,
+                "total_time": None,
+                "lines": [],
+            }
+        if is_data:
+            if file_pattern.match(line):
+                buf["file_name"] = file_pattern.match(line).groups()[0]
+            if name_pattern.match(line):
+                buf["func_name"] = name_pattern.match(line).groups()[0]
+            if time_pattern.match(line):
+                buf["total_time"] = time_pattern.match(line).groups()[0]
+
+            # get line profile data
+            if line_pattern.match(line):
+                if not is_line_founded:
+                    code = line_pattern.match(line).groups()[-1]
+                    # save start point of code column. because python code have many space.
+                    code_index = line.find(code)
+                    is_line_founded = True
+                matcher = line_pattern.match(line).groups()
+                line_data = {
+                    "line": matcher[0],
+                    "hits": matcher[1] if matcher[1] else 0,
+                    "time": matcher[2] if matcher[2] else 0,
+                    "code": line[code_index:],
+                }
+                buf["lines"].append(line_data)
+    # least
+    if buf:
+        profile_data_list.append(buf)
+
+    return profile_data_list
+
+
+def persist(db, profile_data):
+    """
+    persistence to sqlite database
+    :param DbClass db:
+    :param list profile_data:
+    :return:
+    """
+    for data in profile_data:
+        # invalid format remove.
+        if not data["func_name"]:
+            continue
+        data_id = db.save_profile_data(
+            file_name=data["file_name"],
+            func_name=data["func_name"],
+            total_time=data["total_time"]
+        )
+        for line in data["lines"]:
+            db.save_line_data(
+                profile_id=data_id,
+                file_name=data["file_name"],
+                func_name=data["func_name"],
+                line=line["line"],
+                hits=line["hits"],
+                time=line["time"],
+                code=line["code"],
+            )
+        db.commit()
+    db.create_indexes()
+    db.commit()
+
+
+def get_max_str_length(raw_list_dict_data, column_name):
+    """
+    max length for specified column.
+    It's useful for pretify report.
+    :param list[dict] raw_list_dict_data:
+    :param str column_name:
+    :return:
+    """
+    max_length = max([len(str(dict_[column_name])) for dict_ in raw_list_dict_data])
+    # I need prettify format. not real max length of data
+    if len(column_name) > max_length:
+        return len(column_name)
+    return max_length
+
+
+def create_report(base_data, columns, report_name):
+    """
+    write report file.
+    :param list[dict] base_data:
+    :param list[str] columns:
+    :param str report_name:
+    :return:
+    """
+    with open(report_name, "w") as f:
+        # get max_length of columns for prettify report.
+        max_length_dict = {x: get_max_str_length(base_data, x) for x in columns}
+
+        # write header
+        header_str = "  ".join([x.rjust(max_length_dict[x]) for x in columns])
+        header_line = "  ".join(["-".rjust(max_length_dict[x], "-") for x in columns])
+        f.write("{}\n".format(header_str))
+        f.write("{}\n".format(header_line))
+
+        # write data
+        for row_data in base_data:
+            row_data_list = []
+            for col in columns:
+                col_data = row_data[col]
+                # str is ljust, digit is rjust
+                if hasattr(col_data, "ljust"):
+                    row_data_list.append(col_data.ljust(max_length_dict[col]))
+                else:
+                    row_data_list.append(str(col_data).rjust(max_length_dict[col]))
+
+            f.write("{}\n".format("  ".join(row_data_list)))
+
+
+def report(db, report_file_name_prefix):
+    """
+    一般的な観点でのレポートを出力する。現時点では以下の2種類
+
+    * 関数ごとの時間
+    * 行ごとの時間
+
+    :param DbClass db: temporary database
+    :param str report_file_name_prefix: report name prefix
+    :return:
+    """
+    # 関数ごとに経過時間や平均などを集計
+    func_list_with_time_sql = """
+    select
+        file_name,
+        func_name,
+        round(sum(total_time),5) as total_time,
+        round(avg(total_time),5) as avg_time,
+        count(*) as call_count
+    from
+        profile_data
+    group by
+        file_name,
+        func_name
+    order by
+        3 desc ,1,2
+    """
+
+    # すべての行単位で集計
+    whole_line_with_time_sql = """
+    select
+        file_name,
+        line,
+        sum(hits) as hits,
+        sum(time) as total_time,
+        (
+            select
+                code
+            from
+                profile_line_data
+            where
+                file_name = d.file_name
+            and line = d.line
+            limit 1
+        ) as code
+    from
+        profile_line_data d
+    group by
+        file_name, line
+    order by
+        1, 2
+    """
+
+    # sqlite.Row is better, but i need dict.(ie. add custom column data)
+    summary_data = [dict(row) for row in db.execute(func_list_with_time_sql).fetchall()]  # type: list[dict]
+    # add graph column
+    line_data = [dict(row) for row in db.execute(whole_line_with_time_sql).fetchall()]  # type: list[dict]
+    # add graph column
+    max_time = max([row["total_time"] for row in line_data])
+    [row.update(graph=round(row["total_time"]*10/max_time)*"*") for row in line_data]
+
+    # create summary report.
+    create_report(
+        base_data=summary_data,
+        columns=["file_name", "func_name", "total_time", "avg_time", "call_count"],
+        report_name="{}_summary_data.log".format(report_file_name_prefix)
+    )
+    # create line report.
+    create_report(
+        base_data=line_data,
+        columns=["file_name", "line", "hits", "total_time", "graph", "code"],
+        report_name="{}_line_data.log".format(report_file_name_prefix)
+    )
+
+
+if __name__ == "__main__":
+    target_file_name, target_db_name, report_name_prefix = init()
+    # テキスト形式のプロファイルログをDictに変換
+    parsed_profile_data = parse(target_file_name)
+    # 一旦SQLiteに格納し、各種集計・分析後にレポート出力
+    with DbClass(target_db_name) as tmp_db:
+        persist(tmp_db, parsed_profile_data)
+        if report_name_prefix:
+            report(tmp_db, report_name_prefix)
+        else:
+            report(tmp_db, target_file_name)
+
+
+
